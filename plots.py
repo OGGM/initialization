5fb178b7c37e1b4e6d74abdd46ef27f01acd9cf3
juliaeis <jeis@uni-bremen.de>
2019-02-06 08:33:54 +0100

    PEP

--- plots.py
+++ plots.py
@@ -1,21 +1,16 @@
-import os
 from functools import partial
 from copy import deepcopy
-
 from pylab import *
-
 import pandas as pd
-pd.options.mode.chained_assignment = None
-
 import matplotlib as mpl
 from matplotlib import cm
 from mpl_toolkits.axes_grid.anchored_artists import AnchoredText
 from matplotlib.collections import LineCollection
 from matplotlib.legend_handler import HandlerLineCollection
-
 from oggm import utils
 from oggm.core.flowline import FluxBasedModel, FileModel
 FlowlineModel = partial(FluxBasedModel, inplace=False)
+pd.options.mode.chained_assignment = None
 
 mpl.rcParams['axes.linewidth'] = 3
 mpl.rcParams['xtick.major.width'] = 3
@@ -23,15 +18,17 @@ mpl.rcParams['ytick.major.width'] = 3
 mpl.rcParams['font.size'] = 30
 mpl.rcParams['font.weight'] = 'medium'
 mpl.rcParams['axes.labelweight'] = 'medium'
-mpl.rcParams['legend.fontsize']= 25 #30
-mpl.rcParams['lines.linewidth']=3
+mpl.rcParams['legend.fontsize'] = 25
+mpl.rcParams['lines.linewidth'] = 3
+
 
 def add_at(ax, t, loc=2):
     fp = dict(size=15)
-    _at = AnchoredText(t, loc=loc, prop=fp,borderpad=0)
+    _at = AnchoredText(t, loc=loc, prop=fp, borderpad=0)
     _at.patch.set_linewidth(3)
     ax.add_artist(_at)
     return _at
+
 
 class HandlerColorLineCollection(HandlerLineCollection):
     def create_artists(self, legend, artist, xdescent, ydescent,
@@ -47,13 +44,17 @@ class HandlerColorLineCollection(HandlerLineCollection):
         lc.set_linewidth(artist.get_linewidth())
         return [lc]
 
-def plot_experiment(gdir,ex_mod,ys, plot_dir):
-    plot_dir = os.path.join(plot_dir,'00_experiment')
+
+def plot_experiment(gdir, ex_mod, ys, plot_dir):
+    plot_dir = os.path.join(plot_dir, '00_experiment')
     utils.mkdir(plot_dir)
-    x = np.arange(ex_mod.fls[-1].nx) * ex_mod.fls[-1].dx * ex_mod.fls[-1].map_dx
+
+    x = np.arange(ex_mod.fls[-1].nx) * ex_mod.fls[-1].dx * \
+        ex_mod.fls[-1].map_dx
+
     grid = plt.GridSpec(2, 1, hspace=0.2, wspace=0.2)
     ax1 = plt.subplot(grid[0, 0])
-    ax2 = plt.subplot(grid[1,0],sharex=ax1)
+    ax2 = plt.subplot(grid[1, 0], sharex=ax1)
 
     if gdir.name != '':
         ax1.set_title(gdir.rgi_id+':'+gdir.name)
@@ -64,15 +65,17 @@ def plot_experiment(gdir,ex_mod,ys, plot_dir):
     ex_mod = deepcopy(ex_mod)
     ex_mod.reset_y0(1850)
     ex_mod.run_until(ys)
-    i = np.where(ex_mod.fls[-1].thick > 0)[0][-1] +10
+    i = np.where(ex_mod.fls[-1].thick > 0)[0][-1] + 10
 
-    ax1.plot(x[:i], ex_mod.fls[-1].surface_h[:i], 'k:',label=r'$x_{'+str(ys)+'}^{exp}$',linewidth=3)
-    ax1.plot(x[:i], ex_mod.fls[-1].bed_h[:i], 'k',label=r'$b$', linewidth=3)
+    ax1.plot(x[:i], ex_mod.fls[-1].surface_h[:i], 'k:',
+             label=r'$x_{'+str(ys)+'}^{exp}$', linewidth=3)
+    ax1.plot(x[:i], ex_mod.fls[-1].bed_h[:i], 'k', label=r'$b$', linewidth=3)
 
     ex_mod.run_until(2000)
 
-    ax2.plot(x[:i], ex_mod.fls[-1].surface_h[:i], 'k:',label=r'$x_{2000}^{exp = obs} $', linewidth=3)
-    ax2.plot(x[:i], ex_mod.fls[-1].bed_h[:i], 'k', label=r'$b$',linewidth=3)
+    ax2.plot(x[:i], ex_mod.fls[-1].surface_h[:i],
+             'k:', label=r'$x_{2000}^{exp = obs} $', linewidth=3)
+    ax2.plot(x[:i], ex_mod.fls[-1].bed_h[:i], 'k', label=r'$b$', linewidth=3)
 
     # add figure names and legends
     add_at(ax1, r"a", loc=3)
@@ -88,28 +91,28 @@ def plot_experiment(gdir,ex_mod,ys, plot_dir):
 
     ax1.tick_params(axis='both', which='major')
     ax2.tick_params(axis='both', which='major')
-    plt.savefig(os.path.join(plot_dir,'experiment_'+str(ys)+'_'+gdir.rgi_id+'2.pdf'), dpi=300)
-    plt.savefig(os.path.join(plot_dir, 'experiment_' + str(ys) + '_' + gdir.rgi_id + '.png'), dpi=300)
+    fig_name = 'experiment_'+str(ys)+'_'+gdir.rgi_id
+    plt.savefig(os.path.join(plot_dir, fig_name+'.pdf'), dpi=300)
+    plt.savefig(os.path.join(plot_dir, fig_name+'.png'), dpi=300)
 
 
 def plot_candidates(gdir, df, yr, plot_dir):
-    plot_dir = os.path.join(plot_dir,'06_candidates')
+    plot_dir = os.path.join(plot_dir, '06_candidates')
     utils.mkdir(plot_dir)
-    fig, ax = plt.subplots(figsize=(10,5))
-    for file in os.listdir(os.path.join(gdir.dir,str(yr))):
+    fig, ax = plt.subplots(figsize=(10, 5))
+    for file in os.listdir(os.path.join(gdir.dir, str(yr))):
         if file.startswith('model_run'+str(yr)+'_random'):
             suffix = file.split('model_run')[1].split('.nc')[0]
-            rp = os.path.join(gdir.dir,str(yr),'model_run'+suffix+'.nc')
+            rp = os.path.join(gdir.dir, str(yr), 'model_run'+suffix+'.nc')
             try:
                 fmod = FileModel(rp)
-                fmod.volume_m3_ts().plot(ax=ax, color='grey', label='', zorder=1)
+                fmod.volume_m3_ts().plot(ax=ax, color='grey', label='',
+                                         zorder=1)
 
             except:
                 pass
 
     # last one again for labeling
-    label = r'temperature bias $\in [$' + str(
-        df['temp_bias'].min()) + ',' + str(df['temp_bias'].max()) + '$]$'
     df.time = df.time.apply(lambda x: int(x))
     t_eq = df['time'].sort_values().iloc[0]
 
@@ -118,11 +121,11 @@ def plot_candidates(gdir, df, yr, plot_dir):
 
     fmod.volume_m3_ts().plot(ax=ax, color='grey', label=None, zorder=1)
     ax.axvline(x=int(t_eq), color='k', zorder=1)
-    if df.objective.min()==df.objective.max():
+    if df.objective.min() == df.objective.max():
         norm = mpl.colors.LogNorm(vmin=0.1, vmax=1e05, clip=True)
         cmap = matplotlib.cm.get_cmap('viridis')
         df.plot.scatter(x='time', y='volume', ax=ax,
-                        color= cmap(norm(df.objective.min())),s=250,
+                        color=cmap(norm(df.objective.min())), s=250,
                         edgecolors='k', zorder=2)
         sm = plt.cm.ScalarMappable(cmap=cmap)
         sm.set_array([])
@@ -131,7 +134,10 @@ def plot_candidates(gdir, df, yr, plot_dir):
         cbar.set_label('Fitness value')
     else:
         # colored points
-        df.plot.scatter(x='time', y='volume', ax=ax, c='Fitness value', colormap='viridis', norm=mpl.colors.LogNorm(vmin=0.1, vmax=1e5,clip=True), s=250,edgecolors='k', zorder=2)
+        df.plot.scatter(x='time', y='volume', ax=ax, c='Fitness value',
+                        colormap='viridis',
+                        norm=mpl.colors.LogNorm(vmin=0.1, vmax=1e5,clip=True),
+                        s=250,edgecolors='k', zorder=2)
     plt.xlabel('Time (years)')
     plt.ylabel(r'Volume $(m^3)$')
     plt.show()
@@ -157,8 +163,8 @@ def plot_fitness_over_time(gdir, df_list, ex_mod, plot_dir):
         color = []
         for i in range(len(volumes)):
             if i != len(volumes) - 1:
-                part = df[(df.volume >= volumes[i]) & (
-                df.volume <= volumes[i + 1])]
+                part = df[(df.volume >= volumes[i]) & (df.volume <=
+                                                       volumes[i + 1])]
                 color.append(part.objective.min())
             else:
                 part = df[df.volume >= volumes[i]]
@@ -180,7 +186,7 @@ def plot_fitness_over_time(gdir, df_list, ex_mod, plot_dir):
             else:
                 color = cmap(norm(c))
             ax.add_patch(
-                Rectangle((y-(w/2) , x), w, volumes[1] - volumes[0],
+                Rectangle((y-(w/2), x), w, volumes[1] - volumes[0],
                           color=color))
 
     # add experiment in plot
@@ -198,19 +204,20 @@ def plot_fitness_over_time(gdir, df_list, ex_mod, plot_dir):
     cbar.ax.tick_params(labelsize=25)
     cbar.set_label('Fitness value', fontsize=25)
     plt.savefig(os.path.join(plot_dir, 'starting' '_' + gdir.rgi_id + '.png'))
-    #plt.close()
-    #plt.show()
+    # plt.close()
+    # plt.show()
 
 
-def plot_surface(gdir,df,ex_mod,ys, plot_dir):
+def plot_surface(gdir, df, ex_mod, ys, plot_dir):
 
-    plot_dir = os.path.join(plot_dir,'03_surface_by_fitness')
+    plot_dir = os.path.join(plot_dir, '03_surface_by_fitness')
     utils.mkdir(plot_dir)
-    x = np.arange(ex_mod.fls[-1].nx) * ex_mod.fls[-1].dx * ex_mod.fls[-1].map_dx
-    fig = plt.figure(figsize=(15,10))
+    x = np.arange(ex_mod.fls[-1].nx) * ex_mod.fls[-1].dx * \
+        ex_mod.fls[-1].map_dx
+    fig = plt.figure(figsize=(15, 10))
     grid = plt.GridSpec(2, 2, hspace=0.2, wspace=0.2)
     ax1 = plt.subplot(grid[0, 0])
-    ax2 = plt.subplot(grid[0, 1],sharey=ax1)
+    ax2 = plt.subplot(grid[0, 1], sharey=ax1)
     ax3 = plt.subplot(grid[1, :])
 
     if gdir.name != '':
@@ -235,28 +242,30 @@ def plot_surface(gdir,df,ex_mod,ys, plot_dir):
         model.volume_m3_ts().plot(ax=ax3, color=[color], label='')
         model.run_until(2000)
 
-        ax2.plot(x, model.fls[-1].surface_h, color=color,label='')
+        ax2.plot(x, model.fls[-1].surface_h, color=color, label='')
 
     # plot experiments
     ex_mod = deepcopy(ex_mod)
-    ex_mod.volume_m3_ts().plot(ax=ax3, color='red', linestyle=':', linewidth=3, label='')
+    ex_mod.volume_m3_ts().plot(ax=ax3, color='red', linestyle=':', linewidth=3,
+                               label='')
     ex_mod.reset_y0(1850)
     ex_mod.run_until(ys)
 
-    ax1.plot(x, ex_mod.fls[-1].surface_h, ':', color='red',label='',linewidth=3)
-    ax1.plot(x, ex_mod.fls[-1].bed_h, 'k',label='', linewidth=3)
+    ax1.plot(x, ex_mod.fls[-1].surface_h, ':', color='red', label='',
+             linewidth=3)
+    ax1.plot(x, ex_mod.fls[-1].bed_h, 'k', label='', linewidth=3)
 
     ex_mod.run_until(2000)
 
-    ax2.plot(x, ex_mod.fls[-1].surface_h, ':', color='red',label='', linewidth=3)
-    ax2.plot(x, ex_mod.fls[-1].bed_h, 'k', label='',linewidth=3)
-
+    ax2.plot(x, ex_mod.fls[-1].surface_h, ':', color='red', label='',
+             linewidth=3)
+    ax2.plot(x, ex_mod.fls[-1].bed_h, 'k', label='', linewidth=3)
 
     # add colorbar
     sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
     sm.set_array([])
     cax, kw = mpl.colorbar.make_axes([ax1, ax2, ax3])
-    cbar = fig.colorbar(sm, cax=cax ,**kw)
+    cbar = fig.colorbar(sm, cax=cax, **kw)
     cbar.ax.tick_params()
     cbar.set_label('Fitness value')
 
@@ -275,7 +284,7 @@ def plot_surface(gdir,df,ex_mod,ys, plot_dir):
     ax1.tick_params(axis='both', which='major')
     ax2.tick_params(axis='both', which='major')
     ax3.tick_params(axis='both', which='major')
-    #ax3.yaxis.offsetText.set_fontsize(30)
+    # ax3.yaxis.offsetText.set_fontsize(30)
 
     # add legend
     # legend
@@ -306,26 +315,27 @@ def plot_surface(gdir,df,ex_mod,ys, plot_dir):
     l3 = ax3.legend(handles=[lc3, lc],
                     handler_map={
                         lc: HandlerColorLineCollection(numpoints=100)},
-                    labels=[r'$s_{'+str(ys)+'-2000}^{exp}$', r'$s_{'+str(ys)+'-2000}$'], loc=1)
+                    labels=[r'$s_{'+str(ys)+'-2000}^{exp}$',
+                            r'$s_{'+str(ys)+'-2000}$'], loc=1)
 
     l1.set_zorder(0)
     l2.set_zorder(0)
     l3.set_zorder(0)
 
-
     ax3.set_xlim(xmin=1847, xmax=2003)
-    plt.savefig(os.path.join(plot_dir,'surface_'+str(ys)+'_'+gdir.rgi_id+'.pdf'), dpi=300)
-    plt.savefig(os.path.join(plot_dir, 'surface_' + str(ys) + '_' + gdir.rgi_id + '.png'), dpi=300)
-    #plt.close()
+    fig_name = 'surface_'+str(ys)+'_'+gdir.rgi_id
+    plt.savefig(os.path.join(plot_dir, fig_name+'.pdf'), dpi=300)
+    plt.savefig(os.path.join(plot_dir, fig_name+'.png'), dpi=300)
+    # plt.close()
 
 
-def plot_median(gdir,df,ex_mod,ys, plot_dir):
-    plot_dir = os.path.join(plot_dir,'04_median')
+def plot_median(gdir, df, ex_mod, ys, plot_dir):
+    plot_dir = os.path.join(plot_dir, '04_median')
     utils.mkdir(plot_dir)
     x = np.arange(ex_mod.fls[-1].nx) * ex_mod.fls[-1].dx * ex_mod.fls[
         -1].map_dx
     grid = plt.GridSpec(1, 1, hspace=0.2, wspace=0.2)
-    plt.figure(figsize=(9,4))
+    plt.figure(figsize=(9, 4))
     ax1 = plt.subplot(grid[0, 0])
 
     if gdir.name != '':
@@ -336,20 +346,19 @@ def plot_median(gdir,df,ex_mod,ys, plot_dir):
         plt.suptitle(gdir.rgi_id)
 
     df = df.sort_values('objective', ascending=False)
-    df = df[df.objective<100]
+    df = df[df.objective < 100]
     min_id = df.volume.idxmin()
     max_id = df.volume.idxmax()
 
-    min_model = deepcopy(df.loc[min_id,'model'])
+    min_model = deepcopy(df.loc[min_id, 'model'])
     max_model = deepcopy(df.loc[max_id, 'model'])
 
     min_model.reset_y0(ys)
     max_model.reset_y0(ys)
 
     ax1.fill_between(x, deepcopy(min_model.fls[-1].surface_h),
-                     deepcopy(max_model.fls[-1].surface_h),alpha=0.3,
-                     color='grey',label=r'$\mathcal{S}_{'+str(ys)+'}^{100}$')
-
+                     deepcopy(max_model.fls[-1].surface_h), alpha=0.3,
+                     color='grey', label=r'$\mathcal{S}_{'+str(ys)+'}^{100}$')
 
     # 5% quantile and median of 5% quantile
     quant_df = df[df.objective <= df.objective.quantile(0.05)]
@@ -365,9 +374,8 @@ def plot_median(gdir,df,ex_mod,ys, plot_dir):
                      deepcopy(q_max_model.fls[-1].surface_h), alpha=0.5,
                      label=r'$Q_{0.05}(\mathcal{S}_{'+str(ys)+'}^{100})$')
 
-
     # median of 5% quantile
-    quant_df.loc[:,'length'] = quant_df.model.apply(lambda x: x.length_m)
+    quant_df.loc[:, 'length'] = quant_df.model.apply(lambda x: x.length_m)
     quant_df = quant_df.sort_values('length', ascending=False)
     l = len(quant_df)
     if l % 2:
@@ -382,7 +390,7 @@ def plot_median(gdir,df,ex_mod,ys, plot_dir):
     ax1.plot(x, median_model.fls[-1].surface_h, label='median',
              linewidth=3)
 
-    #experiment
+    # experiment
     ex_mod = deepcopy(ex_mod)
     ex_mod.reset_y0(1850)
     ex_mod.run_until(ys)
@@ -397,14 +405,13 @@ def plot_median(gdir,df,ex_mod,ys, plot_dir):
     ax1.set_ylabel('Altitude (m)')
     ax1.set_xlabel('Distance along the main flowline (m)')
 
-
     ax1.tick_params(axis='both', which='major')
 
     l1 = ax1.legend(loc=1)
     l1.set_zorder(1)
+    fig_name = 'median_'+str(ys)+'_'+gdir.rgi_id
+    plt.savefig(os.path.join(plot_dir, fig_name+'.pdf'), dpi=300)
+    plt.savefig(os.path.join(plot_dir, fig_name+'.png'), dpi=300)
 
-    plt.savefig(os.path.join(plot_dir,'median_'+str(ys)+'_'+gdir.rgi_id+'.pdf'), dpi=300)
-    plt.savefig(os.path.join(plot_dir, 'median_' + str(ys)+'_'+gdir.rgi_id + '.png'), dpi=300)
-
-    #plt.close()
+    # plt.close()
     return median_model
